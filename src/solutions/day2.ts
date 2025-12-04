9718d67e340ab56e1d3c877073050b21fba27429
Fred Luetkemeier <fredluetkemeier@gmail.com>
2025-12-04 05:53:29 -0600

    day 2 part 2 solution

--- src/solutions/day2.ts
+++ src/solutions/day2.ts
@@ -3,7 +3,7 @@ export function dayTwo(input: string): [string, string] {
         .split(",")
         .map((x) => x.split("-").map(Number) as IdRange);
 
-    return [partOne(idRanges).toString(), ""];
+    return [partOne(idRanges).toString(), partTwo(idRanges).toString()];
 }
 
 // PART 1
@@ -14,7 +14,7 @@ function partOne(idRanges: IdRange[]): number {
     const invalidIds = idRanges.reduce(
         (acc: number[], range: IdRange) => [
             ...acc,
-            ...findInvalidNumbers(range),
+            ...findSymmetricalIds(range),
         ],
         []
     );
@@ -22,12 +22,12 @@ function partOne(idRanges: IdRange[]): number {
     return invalidIds.reduce((acc, value) => acc + value, 0);
 }
 
-function findInvalidNumbers(range: IdRange): number[] {
+function findSymmetricalIds(range: IdRange): number[] {
     const [start, end] = range;
     let invalidNumbers: number[] = [];
 
     for (let i = start; i <= end; i++) {
-        if (isNumberInvalid(i.toString())) {
+        if (isIdSymmetrical(i.toString())) {
             invalidNumbers = [...invalidNumbers, i];
         }
     }
@@ -35,7 +35,7 @@ function findInvalidNumbers(range: IdRange): number[] {
     return invalidNumbers;
 }
 
-function isNumberInvalid(value: string): boolean {
+function isIdSymmetrical(value: string): boolean {
     const digits = value.split("");
 
     if (digits.length % 2 !== 0) return false;
@@ -45,4 +45,50 @@ function isNumberInvalid(value: string): boolean {
     const secondHalf = digits.slice(middleIndex).join("");
 
     return firstHalf === secondHalf;
+}
+
+// PART 2
+
+function partTwo(idRanges: IdRange[]): number {
+    const invalidIds = idRanges.reduce(
+        (acc: number[], range: IdRange) => [...acc, ...findInvalidIds(range)],
+        []
+    );
+
+    return invalidIds.reduce((acc, value) => acc + value, 0);
+}
+
+function findInvalidIds(range: IdRange): number[] {
+    const [start, end] = range;
+    let invalidNumbers: number[] = [];
+
+    for (let i = start; i <= end; i++) {
+        if (isIdInvalid(i.toString())) {
+            invalidNumbers = [...invalidNumbers, i];
+        }
+    }
+
+    return invalidNumbers;
+}
+
+function isIdInvalid(value: string, windowSize: number = 1): boolean {
+    const digits = value.split("");
+
+    const maxWindowSize = Math.floor(digits.length / 2);
+
+    if (windowSize > maxWindowSize) {
+        return false;
+    }
+
+    const windowPattern = digits.slice(0, windowSize).join("");
+
+    for (let i = windowSize; i < digits.length; i += windowSize) {
+        const windowSlice = digits.slice(i, i + windowSize).join("");
+
+        if (windowPattern !== windowSlice) {
+            return isIdInvalid(value, windowSize + 1);
+        }
+    }
+
+    return true;
 }
