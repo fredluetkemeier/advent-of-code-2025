94af637a953da262cac147066ffa9e30b2ec9174
Fred Luetkemeier <fredluetkemeier@gmail.com>
2025-12-03 23:54:43 -0600

    Refactor part 2

--- src/solutions/day1.ts
+++ src/solutions/day1.ts
@@ -1,5 +1,5 @@
 export function dayOne(inputs: string[]): [string, string] {
-    return ["", partTwo(inputs).toString()];
+    return [partOne(inputs).toString(), partTwo(inputs).toString()];
 }
 
 // PART 1
@@ -12,81 +12,11 @@ function partOne(inputs: string[], dialPosition: number = 50): number {
     const rotation = parseDialRotation(value!);
     const newPosition = rotateDial(dialPosition, rotation);
 
-    return (newPosition === 0 ? 1 : 0) + partOne(rest ?? [], newPosition);
-}
-
-// PART 2
-
-function partTwo(inputs: string[]): number {
-    const rotations = parseDialRotations(inputs);
-
-    let dialPosition = 50;
-    let zeroCount = 0;
-
-    for (const [direction, distance] of rotations) {
-        zeroCount += Math.floor(distance / 100);
-
-        const adjustedDistance = distance % 100;
-
-        if (direction === "left") {
-            const positionValue = dialPosition - adjustedDistance;
-
-            if (positionValue < 0) {
-                const prevPosition = dialPosition;
-                dialPosition = 100 + positionValue;
-
-                if (dialPosition !== 0 && prevPosition !== 0) {
-                    zeroCount += 1;
-                }
-            } else {
-                dialPosition = positionValue;
-            }
-
-            if (dialPosition === 0) {
-                zeroCount += 1;
-            }
-        } else {
-            const positionValue = dialPosition + adjustedDistance;
-
-            if (positionValue > 99) {
-                const prevPosition = dialPosition;
-                dialPosition = positionValue - 100;
-
-                if (dialPosition !== 0 && prevPosition !== 0) {
-                    zeroCount += 1;
-                }
-            } else {
-                dialPosition = positionValue;
-            }
-
-            if (dialPosition === 0) {
-                zeroCount += 1;
-            }
-        }
-    }
-
-    return zeroCount;
-}
-
-// SHARED
-
-type Direction = "left" | "right";
-
-function parseDialRotations(values: string[]): [Direction, number][] {
-    return values.map((x) => [
-        x.slice(0, 1) === "L" ? "left" : "right",
-        Number(x.slice(1)),
-    ]);
-}
-
-function parseDialRotation(value: string): number {
-    const parsedRotation = value.slice(1);
-    return Number(value.slice(0, 1) === "L" ? -parsedRotation : parsedRotation);
+    return (newPosition === 0 ? 1 : 0) + partOne(rest, newPosition);
 }
 
 function rotateDial(position: number, rotation: number): number {
-    const adjustedRotation =
-        Math.abs(rotation) > 100 ? rotation % 100 : rotation;
+    const adjustedRotation = rotation % 100;
     const positionValue = position + adjustedRotation;
 
     if (positionValue < 0) return 100 + positionValue;
@@ -94,4 +24,63 @@ function rotateDial(position: number, rotation: number): number {
     if (positionValue > 99) return positionValue - 100;
 
     return positionValue;
+}
+
+// PART 2
+
+function partTwo(inputs: string[], dialPosition: number = 50): number {
+    if (inputs.length == 0) return 0;
+
+    const [value, ...rest] = inputs;
+
+    const rotation = parseDialRotation(value!);
+
+    const adjustedDistance = rotation % 100;
+
+    const adjustedDialPosition = dialPosition + adjustedDistance;
+    const newDialPosition =
+        adjustedDialPosition < 0 || adjustedDialPosition > 99
+            ? wrapDialPosition(adjustedDialPosition)
+            : adjustedDialPosition;
+
+    return (
+        countZeroes(
+            dialPosition,
+            rotation,
+            adjustedDialPosition,
+            newDialPosition
+        ) + partTwo(rest, newDialPosition)
+    );
+}
+
+function countZeroes(
+    dialPosition: number,
+    rotation: number,
+    adjustedDialPosition: number,
+    newDialPosition: number
+): number {
+    const fullRotationZeroCount = Math.floor(Math.abs(rotation) / 100);
+    const isDialOutOfBounds =
+        adjustedDialPosition < 0 || adjustedDialPosition > 99;
+
+    if (isDialOutOfBounds && dialPosition !== 0 && newDialPosition !== 0) {
+        return 1 + fullRotationZeroCount;
+    }
+
+    if (newDialPosition === 0) {
+        return 1 + fullRotationZeroCount;
+    }
+
+    return fullRotationZeroCount;
+}
+
+function wrapDialPosition(dialPosition: number): number {
+    return dialPosition < 0 ? 100 + dialPosition : dialPosition - 100;
+}
+
+// SHARED
+
+function parseDialRotation(value: string): number {
+    const parsedRotation = value.slice(1);
+    return Number(value.slice(0, 1) === "L" ? -parsedRotation : parsedRotation);
 }
