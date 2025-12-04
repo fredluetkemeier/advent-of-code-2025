88f57ed1b85d7b2cac6a5fc7c7a9a5d18d7fcdc5
Fred Luetkemeier <fredluetkemeier@gmail.com>
2025-12-03 20:51:45 -0600

    Part two solution

--- src/solutions/day1.ts
+++ src/solutions/day1.ts
@@ -1,5 +1,5 @@
 export function dayOne(inputs: string[]): [string, string] {
-    return [partOne(inputs).toString(), partTwo(inputs).toString()];
+    return ["", partTwo(inputs).toString()];
 }
 
 // PART 1
@@ -17,37 +17,67 @@ function partOne(inputs: string[], dialPosition: number = 50): number {
 
 // PART 2
 
-function partTwo(inputs: string[], dialPosition: number = 50): number {
-    if (inputs.length == 0) return 0;
+function partTwo(inputs: string[]): number {
+    const rotations = parseDialRotations(inputs);
 
-    const [value, ...rest] = inputs;
+    let dialPosition = 50;
+    let zeroCount = 0;
 
-    const rotation = parseDialRotation(value!);
-    const newPosition = rotateDial(dialPosition, rotation);
+    for (const [direction, distance] of rotations) {
+        zeroCount += Math.floor(distance / 100);
 
-    const zeroTouchCount = countZeroTouches(
-        dialPosition,
-        rotation,
-        newPosition
-    );
+        const adjustedDistance = distance % 100;
 
-    // console.log({ dialPosition, newPosition });
-    console.log("touch count:", zeroTouchCount);
+        if (direction === "left") {
+            const positionValue = dialPosition - adjustedDistance;
 
-    return (
-        (newPosition === 0 ? 1 : 0) +
-        zeroTouchCount +
-        partTwo(rest ?? [], newPosition)
-    );
+            if (positionValue < 0) {
+                const prevPosition = dialPosition;
+                dialPosition = 100 + positionValue;
+
+                if (dialPosition !== 0 && prevPosition !== 0) {
+                    zeroCount += 1;
+                }
+            } else {
+                dialPosition = positionValue;
+            }
+
+            if (dialPosition === 0) {
+                zeroCount += 1;
+            }
+        } else {
+            const positionValue = dialPosition + adjustedDistance;
+
+            if (positionValue > 99) {
+                const prevPosition = dialPosition;
+                dialPosition = positionValue - 100;
+
+                if (dialPosition !== 0 && prevPosition !== 0) {
+                    zeroCount += 1;
+                }
+            } else {
+                dialPosition = positionValue;
+            }
+
+            if (dialPosition === 0) {
+                zeroCount += 1;
+            }
+        }
+    }
+
+    return zeroCount;
 }
 
-function countZeroTouches(
-    position: number,
-    rotation: number,
-    newPosition: number
-): number {}
-
 // SHARED
+
+type Direction = "left" | "right";
+
+function parseDialRotations(values: string[]): [Direction, number][] {
+    return values.map((x) => [
+        x.slice(0, 1) === "L" ? "left" : "right",
+        Number(x.slice(1)),
+    ]);
+}
 
 function parseDialRotation(value: string): number {
     const parsedRotation = value.slice(1);
