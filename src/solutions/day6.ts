c81b395b1aa3a24c69b03928d175f8291e16ba8e
Fred Luetkemeier <fredluetkemeier@gmail.com>
2025-12-09 07:01:32 -0600

    day 6 part 2 checkpoint

--- src/solutions/day6.ts
+++ src/solutions/day6.ts
@@ -1,10 +1,14 @@
 export function daySix(input: string): [string, string] {
-    const { numbers, operators } = parseInput(input);
+    const partOneInput = parsePartOneInput(input);
+    const partTwoInput = parsePartTwoInput(input);
 
-    return [partOne(numbers, operators).toString(), ""];
+    return [
+        partOne(partOneInput.numbers, partOneInput.operators).toString(),
+        partTwo(partTwoInput.numbers, partTwoInput.operators).toString(),
+    ];
 }
 
-function parseInput(input: string): {
+function parsePartOneInput(input: string): {
     numbers: number[][];
     operators: string[];
 } {
@@ -17,6 +21,36 @@ function parseInput(input: string): {
                 .split(" ")
                 .filter((x) => x.trim())
                 .map((x) => Number(x.trim()))
+        ),
+        operators: allRows[allRows.length - 1]!.split(" ").filter((x) =>
+            x.trim()
+        ),
+    };
+}
+
+function parsePartTwoInput(input: string): {
+    numbers: string[][];
+    operators: string[];
+} {
+    const allRows = input.trim().split("\n");
+    const operatorIndices = allRows[allRows.length - 1]!.split("")
+        .map((x, index) => (x === "*" || x === "+" ? index : null))
+        .filter((x) => x !== null);
+
+    return {
+        numbers: allRows.slice(0, -1).map((row) =>
+            row
+                .split("")
+                .map((char, index) =>
+                    char === " " &&
+                    !operatorIndices
+                        .map((operatorIndex) => operatorIndex - 1)
+                        .includes(index)
+                        ? "0"
+                        : char
+                )
+                .join("")
+                .split(" ")
         ),
         operators: allRows[allRows.length - 1]!.split(" ").filter((x) =>
             x.trim()
@@ -40,10 +74,35 @@ function partOne(grid: number[][], operators: string[]): number {
     );
 }
 
-function invertGrid(grid: number[][]): number[][] {
-    return grid.reduce<number[][]>(
+// PART 2
+
+function partTwo(grid: string[][], operators: string[]): number {
+    return invertGrid(grid).reduce(
+        (acc, row, index) => acc + doCephalopodMath(row, operators[index]!),
+        0
+    );
+}
+
+function doCephalopodMath(row: string[], operator: string) {
+    console.log("ROW", row);
+
+    const res = invertGrid(row.map((x) => String(x).split("").reverse()))
+        .map((x) => Number(x.filter((digit) => digit !== "0").join("")))
+        .map((x) => {
+            console.log(x);
+            return x;
+        })
+        .reduce((acc, x) => (operator === "+" ? acc + x : (acc || 1) * x), 0);
+
+    return res;
+}
+
+// SHARE
+
+function invertGrid<T>(grid: T[][]): T[][] {
+    return grid.reduce<T[][]>(
         (yAcc, row) =>
-            row.reduce<number[][]>((xAcc, item, xIndex) => {
+            row.reduce<T[][]>((xAcc, item, xIndex) => {
                 const accRow = yAcc[xIndex];
 
                 return accRow
