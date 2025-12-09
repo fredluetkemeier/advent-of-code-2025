5b0cc19e83a84935fff7308d96dbfc32824d6ff9
Fred Luetkemeier <fredluetkemeier@gmail.com>
2025-12-09 00:21:35 -0600

    day 4 part 2 solution

--- src/solutions/day4.ts
+++ src/solutions/day4.ts
@@ -1,7 +1,7 @@
 export function dayFour(input: string): [string, string] {
     const grid = parseGrid(input);
 
-    return [partOne(grid).toString(), ""];
+    return [partOne(grid).toString(), partTwo(grid).toString()];
 }
 
 type Grid = string[][];
@@ -67,4 +67,59 @@ function getPoints(
 
 function getIndices(startIndex: number, radius: number): number[] {
     return [...Array(3)].map((_, index) => startIndex - radius + index);
+}
+
+// PART 2
+
+function partTwo(grid: Grid): number {
+    const result = removeAccessibleGridSlots(grid);
+
+    if (result.count === 0) return 0;
+
+    return result.count + partTwo(result.newGrid);
+}
+
+function removeAccessibleGridSlots(
+    grid: Grid,
+    rowIndex: number = 0
+): { count: number; newGrid: Grid } {
+    const row = grid[rowIndex];
+
+    if (!row) return { count: 0, newGrid: [] };
+
+    const RADIUS = 1;
+    const maxX = row.length - 1;
+    const maxY = grid.length - 1;
+
+    const { count, newRow } = row.reduce<{ count: number; newRow: string[] }>(
+        (acc, slot, columnIndex) => {
+            if (slot === ".")
+                return { count: acc.count, newRow: [...acc.newRow, slot] };
+
+            const count = getPoints(columnIndex, rowIndex, RADIUS)
+                .filter(([x, y]) => x >= 0 && y >= 0 && x <= maxX && y <= maxY)
+                .reduce(
+                    (acc, [x, y]) => acc + (grid[y]![x] === "@" ? 1 : 0),
+                    0
+                );
+
+            return count < 4
+                ? {
+                      count: acc.count + 1,
+                      newRow: [...acc.newRow, "."],
+                  }
+                : {
+                      count: acc.count,
+                      newRow: [...acc.newRow, slot],
+                  };
+        },
+        { count: 0, newRow: [] }
+    );
+
+    const next = removeAccessibleGridSlots(grid, rowIndex + 1);
+
+    return {
+        count: count + next.count,
+        newGrid: [newRow, ...next.newGrid],
+    };
 }
