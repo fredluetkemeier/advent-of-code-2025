b6577556a2bd9af34579f5084a949ef9d33ecd05
Fred Luetkemeier <fredluetkemeier@gmail.com>
2025-12-10 06:41:01 -0600

    day 7 part 1 checkpoint

--- src/solutions/day7.ts
+++ src/solutions/day7.ts
@@ -1,9 +1,33 @@
 export function daySeven(input: string): [string, string] {
-    return [partOne().toString(), ""];
+    const diagram = parseInput(input);
+
+    return [partOne(diagram).toString(), ""];
+}
+
+type Diagram = string[][];
+
+function parseInput(input: string): Diagram {
+    return input.split("\n").map((x) => x.split(""));
 }
 
 // PART 1
 
-function partOne(): number {
+function partOne(diagram: Diagram): number {
+    const result = projectTachyonBeam(diagram);
+
     return 0;
+}
+
+function projectTachyonBeam(
+    diagram: Diagram,
+    rowIndex: number = 0
+): {
+    splitCount: number;
+    diagram: Diagram;
+} {
+    if (rowIndex > diagram.length)
+        return {
+            splitCount: 0,
+            diagram: diagram,
+        };
 }
