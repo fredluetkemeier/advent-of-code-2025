cac707ff8416fcf71e63a52e441a85feb7ea0fc3
Fred Luetkemeier <fredluetkemeier@gmail.com>
2025-12-13 05:09:56 -0600

    day 8 part 1 checkpoint

--- src/solutions/day8.ts
+++ src/solutions/day8.ts
@@ -1,9 +1,24 @@
 export function dayEight(input: string): [number, number] {
-    return [partOne(), 0];
+    const boxes = parseInput(input);
+
+    return [partOne(boxes), 0];
 }
 
 // PART 1
 
-function partOne(): number {
+function partOne(boxes: Box[]): number {
     return 0;
+}
+
+type Box = {
+    x: number;
+    y: number;
+    z: number;
+};
+
+function parseInput(input: string): Box[] {
+    return input.split("\n").map((row) => {
+        const [x, y, z] = row.split(",");
+        return { x: Number(x!), y: Number(y!), z: Number(z!) };
+    });
 }
