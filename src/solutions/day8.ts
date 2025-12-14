9ab32dd79a0ddfa498bd1dcf30b8ae4b3f76025a
Fred Luetkemeier <fredluetkemeier@gmail.com>
2025-12-13 19:10:00 -0600

    day 8 part 1 checkpoint

--- src/solutions/day8.ts
+++ src/solutions/day8.ts
@@ -4,12 +4,6 @@ export function dayEight(input: string): [number, number] {
     return [partOne(boxes), 0];
 }
 
-// PART 1
-
-function partOne(boxes: Box[]): number {
-    return 0;
-}
-
 type Box = {
     x: number;
     y: number;
@@ -21,4 +15,38 @@ function parseInput(input: string): Box[] {
         const [x, y, z] = row.split(",");
         return { x: Number(x!), y: Number(y!), z: Number(z!) };
     });
+}
+
+// PART 1
+
+function partOne(boxes: Box[]): number {
+    findClosestBoxes(boxes);
+
+    return 0;
+}
+
+function findClosestBoxes(availableBoxes: Box[]) {
+    const temp = availableBoxes.reduce<[Box, Box][]>(
+        (accA, boxA) => [
+            ...accA,
+            ...availableBoxes.reduce<[Box, Box][]>(
+                (accB, boxB) => [...accB, [boxA, boxB]],
+                []
+            ),
+        ],
+        []
+    );
+
+    console.log(temp);
+
+    const sortedBoxes = availableBoxes.sort(
+        (a, b) =>
+            Math.pow(a.x - b.x, 2) +
+            Math.pow(a.y - b.y, 2) +
+            Math.pow(a.z - b.z, 2)
+    );
+
+    console.log(sortedBoxes);
+
+    return [];
 }
