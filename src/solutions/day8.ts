96537f58395436727a8df7bc756c3e16160fc278
Fred Luetkemeier <fredluetkemeier@gmail.com>
2025-12-15 06:16:09 -0600

    day 8 part 2 solution

--- src/solutions/day8.ts
+++ src/solutions/day8.ts
@@ -110,33 +110,61 @@ function makeCircuits(boxPairs: BoxPair[]): Set<string>[] {
 
 function partTwo(boxes: Box[]): number {
     const boxPairs = getClosestBoxPairs(boxes);
+    const {
+        boxes: [{ x: xA }, { x: xB }],
+    } = findPairThatCompletesCircuit(boxPairs, boxes.length)!;
 
-    return 0;
+    return xA * xB;
 }
 
-function findPairThatCompletesCircuit(boxPairs: BoxPair[]): Set<string>[] {
-    return boxPairs.reduce<Set<string>[]>((acc, { boxes: [boxA, boxB] }) => {
-        const [circuitsWithPair, rest] = acc.reduce<
-            [Set<string>[], Set<string>[]]
-        >(
-            (acc, x) =>
-                x.has(boxA.id) || x.has(boxB.id)
-                    ? [[...acc[0], x], acc[1]]
-                    : [acc[0], [...acc[1], x]],
-            [[], []]
-        );
+function findPairThatCompletesCircuit(
+    boxPairs: BoxPair[],
+    boxCount: number
+): BoxPair | undefined {
+    const { pair } = boxPairs.reduce<{
+        circuits: Set<string>[];
+        pair: BoxPair | undefined;
+    }>(
+        (acc, boxPair) => {
+            if (acc.pair) return acc;
 
-        const boxIds = [boxA, boxB].map((x) => x.id);
+            const {
+                boxes: [boxA, boxB],
+            } = boxPair;
 
-        if (circuitsWithPair.length > 0) {
-            const newCircuit = circuitsWithPair.reduce(
-                (acc, circuit) => acc.union(circuit),
-                new Set(boxIds)
+            const [circuitsWithPair, rest] = acc.circuits.reduce<
+                [Set<string>[], Set<string>[]]
+            >(
+                (acc, x) =>
+                    x.has(boxA.id) || x.has(boxB.id)
+                        ? [[...acc[0], x], acc[1]]
+                        : [acc[0], [...acc[1], x]],
+                [[], []]
             );
 
-            return [...rest, newCircuit];
-        }
+            const boxIds = [boxA, boxB].map((x) => x.id);
+            const newCircuits =
+                circuitsWithPair.length > 0
+                    ? [
+                          ...rest,
+                          circuitsWithPair.reduce(
+                              (acc, circuit) => acc.union(circuit),
+                              new Set(boxIds)
+                          ),
+                      ]
+                    : [...acc.circuits, new Set(boxIds)];
 
-        return [...acc, new Set(boxIds)];
-    }, []);
+            return {
+                circuits: newCircuits,
+                pair:
+                    newCircuits.length === 1 &&
+                    newCircuits[0]?.size === boxCount
+                        ? boxPair
+                        : undefined,
+            };
+        },
+        { circuits: [], pair: undefined }
+    );
+
+    return pair;
 }
