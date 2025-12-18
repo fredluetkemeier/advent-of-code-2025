9bacc3171947831addd50a7b504213fffa25d998
Fred Luetkemeier <fredluetkemeier@gmail.com>
2025-12-18 04:16:58 -0600

    checkpoint

--- src/solutions/day9.ts
+++ src/solutions/day9.ts
@@ -42,18 +42,14 @@ function findLargestArea(points: Point[]): number {
 // PART 2
 
 function partTwo(points: Point[]): number {
-    const largestArea = findLargestAreaWithinBounds(points);
-
-    console.log(largestArea);
-
-    return 0;
+    return findLargestAreaWithinBounds(points);
 }
 
 function findLargestAreaWithinBounds(points: Point[]): number {
-    const bounds = makeBounds(points);
+    // const bounds = makeBounds(points);
     const areaSet = new Set<number>();
 
-    const tempMap = new Map<string, number>();
+    // const tempMap = new Map<string, number>();
 
     for (const pointA of points) {
         for (const pointB of points) {
@@ -61,7 +57,7 @@ function findLargestAreaWithinBounds(points: Point[]): number {
 
             const isWithinBounds = isRectangleWithinBounds(
                 [pointA, pointB],
-                bounds
+                points
             );
 
             if (!isWithinBounds) continue;
@@ -71,113 +67,155 @@ function findLargestAreaWithinBounds(points: Point[]): number {
                 (Math.abs(pointA.y - pointB.y) + 1);
 
             areaSet.add(area);
-            tempMap.set(
-                `${pointA.x},${pointA.y}-${pointB.x},${pointB.y}`,
-                area
-            );
+            // tempMap.set(
+            //     `${pointA.x},${pointA.y}-${pointB.x},${pointB.y}`,
+            //     area
+            // );
         }
     }
 
-    console.log(tempMap);
+    // console.log(tempMap);
 
     return [...areaSet].sort((a, b) => b - a)[0]!;
 }
 
-function makeBounds(points: Point[], index: number = 0): [Point, Point][] {
-    if (index === points.length - 1) {
-        return [[points[points.length - 1]!, points[0]!]];
-    }
-
-    return [
-        [points[index]!, points[index + 1]!],
-        ...makeBounds(points, index + 1),
-    ];
-}
-
 function isRectangleWithinBounds(
-    rectangle: [Point, Point],
-    bounds: [Point, Point][]
+    corners: [Point, Point],
+    points: Point[]
 ): boolean {
-    const [pointA, pointB] = rectangle;
-    const extrapolatedPoints = [
-        { x: pointA.x, y: pointB.y },
-        { x: pointB.x, y: pointA.y },
+    const [lowerXBound, upperXBound] = corners
+        .sort((a, b) => a.x - b.x)
+        .map((x) => x.x);
+    const [lowerYBound, upperYBound] = corners
+        .sort((a, b) => a.y - b.y)
+        .map((x) => x.y);
+
+    const allCornerPoints = [
+        ...corners,
+        { x: corners[0].x, y: corners[1].y },
+        { x: corners[1].x, y: corners[0].y },
     ];
-    const boundXMap = makeBoundXMap(bounds);
 
-    return extrapolatedPoints.every((point) => {
-        const intersectingBounds = bounds.filter(([pointA, pointB]) => {
-            if (pointA.x === pointB.x) return false;
+    const isEveryPointOutsideOfRectangle = points.every((point) => {
+        if (allCornerPoints.some((x) => x.x === point.x && x.y === point.y))
+            return true;
 
-            if (
-                (point.x === pointA.x && point.y === pointA.y) ||
-                (point.x === pointB.x && point.y === pointB.y)
-            )
-                return false;
+        // if (
+        //     allCornerPoints.every((x) => {
+        //         const isEqualToPoint = x.x === point.x && x.y === point.y;
+        //         const isNotOutsideOfShape = x.x
 
-            const [lesserPoint, greaterPoint] = [pointA, pointB].sort(
-                (a, b) => a.x - b.x
-            );
+        //         return isEqualToPoint;
+        //     })
+        // )
+        //     return true;
 
-            const boundsAtX = boundXMap.get(point.x)!;
-            const verticalBound = boundsAtX.find(
-                (boundAtX) =>
-                    boundAtX[0].x === point.x &&
-                    boundAtX[1].x === point.x &&
-                    (boundAtX[0].x === lesserPoint!.x ||
-                        boundAtX[0].x === greaterPoint!.x)
-            );
-            const complementaryBound = verticalBound
-                ? boundsAtX.find((boundAtX) => {
-                      const sortedPoints = boundAtX.sort((a, b) => a.x - b.x);
+        const isBetweenXBounds =
+            point.x >= lowerXBound! && point.x <= upperXBound!;
+        const isBetweenYBounds =
+            point.y >= lowerYBound! && point.y <= upperYBound!;
 
-                      const touchesVerticalBound = boundAtX.some(
-                          (x) => x.x === verticalBound![0].x
-                      );
-                      const isNotParallelToOriginalBound =
-                          lesserPoint!.x === sortedPoints[1].x ||
-                          greaterPoint!.x === sortedPoints[0].x;
-                      const isVertical = boundAtX[0].x === boundAtX[1].x;
-
-                      return (
-                          touchesVerticalBound &&
-                          isNotParallelToOriginalBound &&
-                          !isVertical
-                      );
-                  })
-                : undefined;
-
-            const isHigherBoundAtVerticalIntersection =
-                Boolean(complementaryBound) &&
-                lesserPoint!.y < complementaryBound![0].y;
-
-            return (
-                point.x >= lesserPoint!.x &&
-                point.x <= greaterPoint!.x &&
-                point.y > lesserPoint!.y &&
-                (Boolean(complementaryBound)
-                    ? isHigherBoundAtVerticalIntersection
-                    : true)
-            );
-        });
-
-        return (
-            intersectingBounds.length !== 0 &&
-            intersectingBounds.length % 2 !== 0
-        );
+        return !(isBetweenXBounds && isBetweenYBounds);
     });
+
+    return isEveryPointOutsideOfRectangle;
 }
 
-function makeBoundXMap(
-    bounds: [Point, Point][]
-): Map<number, [Point, Point][]> {
-    const map = new Map<number, [Point, Point][]>();
+// function makeBounds(points: Point[], index: number = 0): [Point, Point][] {
+//     if (index === points.length - 1) {
+//         return [[points[points.length - 1]!, points[0]!]];
+//     }
 
-    for (const bound of bounds) {
-        bound.forEach((point) =>
-            map.set(point.x, [...(map.get(point.x) ?? []), bound])
-        );
-    }
+//     return [
+//         [points[index]!, points[index + 1]!],
+//         ...makeBounds(points, index + 1),
+//     ];
+// }
 
-    return map;
-}
+// function isRectangleWithinBounds(
+//     rectangle: [Point, Point],
+//     bounds: [Point, Point][]
+// ): boolean {
+//     const [pointA, pointB] = rectangle;
+//     const extrapolatedPoints = [
+//         { x: pointA.x, y: pointB.y },
+//         { x: pointB.x, y: pointA.y },
+//     ];
+//     const boundXMap = makeBoundXMap(bounds);
+
+//     return extrapolatedPoints.every((point) => {
+//         const intersectingBounds = bounds.filter(([pointA, pointB]) => {
+//             if (pointA.x === pointB.x) return false;
+
+//             if (
+//                 (point.x === pointA.x && point.y === pointA.y) ||
+//                 (point.x === pointB.x && point.y === pointB.y)
+//             )
+//                 return false;
+
+//             const [lesserPoint, greaterPoint] = [pointA, pointB].sort(
+//                 (a, b) => a.x - b.x
+//             );
+
+//             const boundsAtX = boundXMap.get(point.x)!;
+//             const verticalBound = boundsAtX.find(
+//                 (boundAtX) =>
+//                     boundAtX[0].x === point.x &&
+//                     boundAtX[1].x === point.x &&
+//                     (boundAtX[0].x === lesserPoint!.x ||
+//                         boundAtX[0].x === greaterPoint!.x)
+//             );
+//             const complementaryBound = verticalBound
+//                 ? boundsAtX.find((boundAtX) => {
+//                       const sortedPoints = boundAtX.sort((a, b) => a.x - b.x);
+
+//                       const touchesVerticalBound = boundAtX.some(
+//                           (x) => x.x === verticalBound![0].x
+//                       );
+//                       const isNotParallelToOriginalBound =
+//                           lesserPoint!.x === sortedPoints[1].x ||
+//                           greaterPoint!.x === sortedPoints[0].x;
+//                       const isVertical = boundAtX[0].x === boundAtX[1].x;
+
+//                       return (
+//                           touchesVerticalBound &&
+//                           isNotParallelToOriginalBound &&
+//                           !isVertical
+//                       );
+//                   })
+//                 : undefined;
+
+//             const isHigherBoundAtVerticalIntersection =
+//                 Boolean(complementaryBound) &&
+//                 lesserPoint!.y < complementaryBound![0].y;
+
+//             return (
+//                 point.x >= lesserPoint!.x &&
+//                 point.x <= greaterPoint!.x &&
+//                 point.y > lesserPoint!.y &&
+//                 (Boolean(complementaryBound)
+//                     ? isHigherBoundAtVerticalIntersection
+//                     : true)
+//             );
+//         });
+
+//         return (
+//             intersectingBounds.length !== 0 &&
+//             intersectingBounds.length % 2 !== 0
+//         );
+//     });
+// }
+
+// function makeBoundXMap(
+//     bounds: [Point, Point][]
+// ): Map<number, [Point, Point][]> {
+//     const map = new Map<number, [Point, Point][]>();
+
+//     for (const bound of bounds) {
+//         bound.forEach((point) =>
+//             map.set(point.x, [...(map.get(point.x) ?? []), bound])
+//         );
+//     }
+
+//     return map;
+// }
